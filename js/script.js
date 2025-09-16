43264d9be4fbae9baf21e34e4ffb24da618d2042
DianLuca <dianlucca@hotmail.com>
2025-09-16 19:48:24 -0300

    FEAT: Exibindo o elemento nas diferentes páginas

--- js/script.js
+++ js/script.js
@@ -9,71 +9,75 @@ async function executarTarefas() {
     // console.log(usuarios);
 
     const tabelaVagas = document.getElementById("vagas");
-    tabelaVagas.innerHTML = "";
-
-    situacaoVagas.forEach(vagas => {
-        const novaCelulaVaga = tabelaVagas.insertRow();
-
-        // const id = novaCelulaVaga.insertCell();
-        const situacao = novaCelulaVaga.insertCell();
-        const setor = novaCelulaVaga.insertCell();
-        const numero = novaCelulaVaga.insertCell();
-        const nome  = novaCelulaVaga.insertCell();
-        const placa = novaCelulaVaga.insertCell();
-        const horaEntrada = novaCelulaVaga.insertCell();
-        const horaSaida = novaCelulaVaga.insertCell();
-        const valor1 = novaCelulaVaga.insertCell();
-        const button = novaCelulaVaga.insertCell();
-
-        // id.textContent = vagas.id;
-        setor.textContent = vagas.setor;
-        numero.textContent = vagas.numero;
-        nome.textContent = vagas.nome;
-        placa.textContent = vagas.placa;
-        horaEntrada.textContent = vagas.hora_entrada;
-        horaSaida.textContent = vagas.hora_saida;
-        valor1.textContent = vagas.valor;
-        button.innerHTML = "<button>Atualizar</button>";
+    if (tabelaVagas) {
+        tabelaVagas.innerHTML = "";
         
-        if (vagas.situacao == 0) {
-            novaCelulaVaga.style.backgroundColor = "#9c0219ff";
-            situacao.textContent = "Ocupado";
-        } else {
-            novaCelulaVaga.style.backgroundColor = "#272b10";
-            situacao.textContent = "Livre";
-        }
-        
-        button.onclick = async () => {
-            await fetch("../api/vagas/atualizar_vagas.php", {
-                method: "POST",
-                body: JSON.stringify({
-                    id:vagas.id,
-                    id_vaga:vagas.vaga,
-                    situacao:vagas.situacao == 1 ? 0 : 1
-                })
-            });
-            executarTarefas();
-        };
+        situacaoVagas.forEach(vagas => {
+            const novaCelulaVaga = tabelaVagas.insertRow();
+    
+            // const id = novaCelulaVaga.insertCell();
+            const situacao = novaCelulaVaga.insertCell();
+            const setor = novaCelulaVaga.insertCell();
+            const numero = novaCelulaVaga.insertCell();
+            const nome  = novaCelulaVaga.insertCell();
+            const placa = novaCelulaVaga.insertCell();
+            const horaEntrada = novaCelulaVaga.insertCell();
+            const horaSaida = novaCelulaVaga.insertCell();
+            const valor1 = novaCelulaVaga.insertCell();
+            const button = novaCelulaVaga.insertCell();
+    
+            // id.textContent = vagas.id;
+            setor.textContent = vagas.setor;
+            numero.textContent = vagas.numero;
+            nome.textContent = vagas.nome;
+            placa.textContent = vagas.placa;
+            horaEntrada.textContent = vagas.hora_entrada;
+            horaSaida.textContent = vagas.hora_saida;
+            valor1.textContent = vagas.valor;
+            button.innerHTML = "<button>Atualizar</button>";
+            
+            if (vagas.situacao == 0) {
+                novaCelulaVaga.style.backgroundColor = "#9c0219ff";
+                situacao.textContent = "Ocupado";
+            } else {
+                novaCelulaVaga.style.backgroundColor = "#272b10";
+                situacao.textContent = "Livre";
+            }
+            
+            button.onclick = async () => {
+                await fetch("../api/vagas/atualizar_vagas.php", {
+                    method: "POST",
+                    body: JSON.stringify({
+                        id:vagas.id,
+                        id_vaga:vagas.vaga,
+                        situacao:vagas.situacao == 1 ? 0 : 1
+                    })
+                });
+                executarTarefas();
+            };
+    
+            tabelaVagas.appendChild(novaCelulaVaga);
+        });
+    }
 
-        tabelaVagas.appendChild(novaCelulaVaga);
-    });
-
-    // const tabelaUsuarios = document.getElementById("usuarios");
-    // tabelaUsuarios.innerHTML = "";
-
-    // usuarios.forEach(usuario => {
-    //     const novaCelulaUsuario = tabelaUsuarios.insertRow();
-
-    //     const nome = novaCelulaUsuario.insertCell();
-    //     const telefone = novaCelulaUsuario.insertCell();
-    //     const nascimento = novaCelulaUsuario.insertCell();
-
-    //     nome.textContent = usuario.nome;
-    //     telefone.textContent = usuario.telefone;
-    //     nascimento.textContent = usuario.ano_nascimento;
-
-    //     tabelaUsuarios.appendChild(novaCelulaUsuario);
-    // });
+    const tabelaUsuarios = document.getElementById("usuarios");
+    if (tabelaUsuarios) {
+        tabelaUsuarios.innerHTML = "";
+    
+        usuarios.forEach(usuario => {
+            const novaCelulaUsuario = tabelaUsuarios.insertRow();
+    
+            const nome = novaCelulaUsuario.insertCell();
+            const telefone = novaCelulaUsuario.insertCell();
+            const nascimento = novaCelulaUsuario.insertCell();
+    
+            nome.textContent = usuario.nome;
+            telefone.textContent = usuario.telefone;
+            nascimento.textContent = usuario.ano_nasc;
+    
+            tabelaUsuarios.appendChild(novaCelulaUsuario);
+        });
+    }
 }
 
 
